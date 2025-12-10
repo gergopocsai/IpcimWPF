db1cd0ae8712f157d34f05a88b8b618ce270258b
Pocsai Gergő <pocsaig1@kkszki.hu>
2025-12-10 11:52:06 +0100

    Fájl feltöltés, beolvasás, megjelenítés.

--- IpcimWPF/MainWindow.xaml.cs
+++ IpcimWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +15,41 @@ namespace IpcimWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
+    public class Domain
+    {
+        public string domainNev { get; set; }
+        public string ipCim { get; set; }
+
+        public Domain(string domainNev, string ipCim)
+        {
+            this.domainNev = domainNev;
+            this.ipCim = ipCim;
+        }
+    }
+
     public partial class MainWindow : Window
     {
+        
+
+        List<Domain> domainok = new List<Domain>();
         public MainWindow()
         {
+            var sorok = File.ReadAllLines("csudh.txt").Skip(0);
+
             InitializeComponent();
+            foreach (string sor in sorok)
+            {
+                string[] darabok = sor.Split(";");
+                string domainName = darabok[0];
+                string ipAddress = darabok[0];
+
+                domainok.Add(new Domain(domainName, ipAddress));
+            }
+
+            dataGrid.ItemsSource = domainok;
         }
     }
 }
